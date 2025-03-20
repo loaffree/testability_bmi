3315209a37c30de39d195576781a98d6deb1ee89
Alexander Kabanov <kabanov@live.com>
2025-03-21 02:10:05 +0400

    return int

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
     public double calculate(double weight, double height) {
-        return weight / (height * height);
+        return (int) (weight / (height * height));
     }
 }
