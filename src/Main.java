92b00d4945a8b535a0a46abdd86f5b62b94432cc
Alexander Kabanov <kabanov@live.com>
2025-03-21 02:10:05 +0400

    return int

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(82, 1.82);
+        int bmi = (int) service.calculate(98, 1.87);
         System.out.println("Индекс массы тела: " + bmi);
     }
 }
